fb29696e27524c2b6bd6aa5058082b6e688a42c9
jordanburnam <jordanburnam1990@icloud.com>
2015-07-19 18:45:36 -0700

    Added Program.cs

---
+++ RJBMySQLUtilitiesTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RJBMySQLUtilities.DataLogging;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using RJBMySQLUtilities.DataLogging.DataLogging.Controller;
+
+namespace RJBMySQLUtilitiesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string sSourceConnectionString = ConfigurationManager.ConnectionStrings["SourceDatabase"].ConnectionString;
+                string sDestinationConnectionString = ConfigurationManager.ConnectionStrings["DestintaionDatabase"].ConnectionString;
+
+                DataLogger oDataChangeControl = new DataLogger(sSourceConnectionString, sDestinationConnectionString);
+                string sSchema = oDataChangeControl.GetLogSchemaForTable("ObjectsOfHate");
+                int i = 0;
+
+               
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.Write("Press any key to end...");
+            Console.Read();
+         }
+
+      
+    }
+}
